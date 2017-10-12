7c8145db0b23bf854568a9d9adf8de9cc8a6b6c9
fafonso <filipe.afonso@liferay.com>
2017-10-12 16:51:41 +0100

    Add initial commit with clarifai image recognition

---
+++ modules/image-recognition/src/main/java/liferay/ai/image/recognition/ClarifaiIntegrator.java
@@ -0,0 +1,69 @@
+
+package liferay.ai.image.recognition;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+import clarifai2.api.ClarifaiBuilder;
+import clarifai2.api.ClarifaiClient;
+import clarifai2.dto.input.ClarifaiInput;
+import clarifai2.dto.model.output.ClarifaiOutput;
+import clarifai2.dto.prediction.Concept;
+import okhttp3.OkHttpClient;
+
+/**
+ *   @author Filipe Afonso   @author Carlos Hernandez Clarifai API integrator,
+ * centralizes all the code to communicate with clarifai API
+ */
+public class ClarifaiIntegrator {
+
+	/**
+	 * @param bytes
+	 * @return List of predictions
+	 * @throws IOException
+	 * 
+	 * Classify an image, using Clarifai API
+	 */
+	public static List<ClarifaiOutput<Concept>> tagDocument(byte[] bytes)
+		throws IOException {
+
+		// Ensure that we have a Clarifai client
+		ClarifaiClient client = getClient();
+		if (client == null) {
+			return null;
+		}
+		
+		// Ask Clarifai to give us the predictions for the given image
+		List<ClarifaiOutput<Concept>> results =
+			client.getDefaultModels().generalModel().predict().withInputs(
+				ClarifaiInput.forImage(bytes)).executeSync().get();
+		
+		// Note: if we wanted to be asynchronous, we'd execute "execute" instead 
+		// It would have a better performance and we'd get a Future
+		return results;
+	}
+
+	/**
+	 * @return A client for communication with Clarifai API
+	 */
+	public static ClarifaiClient getClient() {
+
+		// Using our user API Key, we initialize a client
+		if ((Constants.appKEY == null)) {
+			_log.error("PLEASE GO TO CLARIFAI's WEBSITE AND CREATE A FREE TEST ACCOUNT");
+			_log.error("Then set your Constants.appKEY");
+			return null;
+		}
+		
+		return new ClarifaiBuilder(Constants.appKEY).
+						client(new OkHttpClient()). // OPTIONAL:  Allows customization of OkHttp by the user
+						buildSync(); // or use .build() to get a Future<ClarifaiClient>
+	}
+
+	private static final Log _log =
+		LogFactoryUtil.getLog(ClarifaiIntegrator.class);
+
+}
---
+++ modules/image-recognition/src/main/java/liferay/ai/image/recognition/ClassifyingDocumentListener.java
@@ -0,0 +1,147 @@
+
+package liferay.ai.image.recognition;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+import com.liferay.asset.kernel.model.AssetEntry;
+import com.liferay.asset.kernel.service.AssetEntryLocalServiceUtil;
+import com.liferay.document.library.kernel.model.DLFileEntry;
+import com.liferay.document.library.kernel.service.DLFileEntryLocalServiceUtil;
+import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.ModelListener;
+
+import clarifai2.dto.model.output.ClarifaiOutput;
+import clarifai2.dto.prediction.Concept;
+
+/**
+ * @author Filipe Afonso
+ * @author Carlos Hernandez
+ *
+ *	Liferay DXP Model Listener for AssetEntry entities
+ */
+@Component(immediate = true, service = ModelListener.class)
+public class ClassifyingDocumentListener extends BaseModelListener<AssetEntry> {
+
+	
+
+	@Activate
+	public void activate(BundleContext context) {
+
+		String location = context.getBundle().getLocation();
+		
+		if (_log.isDebugEnabled()) {
+			_log.debug("bundle " + location + " activated!!!");
+		}
+	}
+
+	@Override
+	public void onAfterCreate(AssetEntry model)
+		throws ModelListenerException {
+
+		if (_log.isDebugEnabled()) {
+			_log.debug("upload document! - onAfterCreate");
+		}
+		
+		// After creating a new asset entry, we try to classify it
+		classifyImage(model);
+		
+		super.onAfterCreate(model);
+	}
+
+	private void classifyImage(AssetEntry model) {
+
+		try {
+			String className = DLFileEntry.class.getName();
+			
+			// We are only classifying DLFileEntry 
+			if (model.getClassName().equals(className)) {
+				long classPK = model.getClassPK();
+				
+				// Get predictions from Clarifai API
+				String[] tagsArray = clarifaiImage(model);
+				
+				if (tagsArray == null) {
+					if (_log.isDebugEnabled()) {
+						_log.debug("Your document won't be tagged, please check everything is alright (API keys etc)");
+					}
+					return;
+				}
+				for (String tag : tagsArray) {
+					if (_log.isDebugEnabled()) {
+						_log.debug(String.format(("tagging document tag: %s"), tag));
+					}
+				}
+				
+				// Apply those predictions as tags
+				long[] categoryIds = null;
+				AssetEntryLocalServiceUtil.updateEntry(
+					model.getUserId(), model.getGroupId(), className, classPK,
+					categoryIds, tagsArray);
+			}
+		}
+		catch (Exception e) {
+			_log.error(e);
+		}
+
+	}
+
+	/**
+	 * @param model
+	 * @return List of predictions from Clarifai API, only applied to uploaded files
+	 */
+	private String[] clarifaiImage(AssetEntry model) { 
+
+		// Initializations
+		List<ClarifaiOutput<Concept>> clarifaiResults =
+			new ArrayList<ClarifaiOutput<Concept>>();
+		
+		List<String> tagList = new ArrayList<String>();
+		
+		try {
+			// Get the uploaded file entry
+			DLFileEntry fileEntry =
+				DLFileEntryLocalServiceUtil.getDLFileEntry(model.getClassPK());
+			byte[] bytes = IOUtils.toByteArray(fileEntry.getContentStream());
+			
+			// Call Clarifai API to classify the uploaded file
+			clarifaiResults = ClarifaiIntegrator.tagDocument(bytes);
+		}
+		catch (Exception e) {
+			_log.error(e);
+		}
+		
+		if (clarifaiResults == null) {
+			return null;
+		}
+		
+		for (ClarifaiOutput<Concept> result : clarifaiResults) {
+			for (Concept data : result.data()) {
+				if (_log.isDebugEnabled()) {
+					_log.debug(String.format("%s=%s", data.name(), data.value()));
+				}
+				
+				// Filter the results, ensuring a high probability of predictions 
+				// being present on the uploaded image 
+				if (data.value() > CLASSIFICATION_THRESHOLD) {
+					tagList.add(data.name());
+				}
+			}
+		}
+		return tagList.toArray(new String[0]);
+	}
+
+	private static final Log _log =
+		LogFactoryUtil.getLog(ClassifyingDocumentListener.class);
+	
+	private static final double CLASSIFICATION_THRESHOLD = 0.9;
+
+}
