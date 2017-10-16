d0d94041603234ca06975754f98b18b94ccc1b5a
fafonso <filipe.afonso@liferay.com>
2017-10-16 17:55:13 +0100

    Update Liferay service calls to avoid using static util methods

--- modules/image-recognition/src/main/java/com/liferay/ai/image/recognition/ClassifyingDocumentListener.java
+++ modules/image-recognition/src/main/java/com/liferay/ai/image/recognition/ClassifyingDocumentListener.java
@@ -10,11 +10,12 @@ import org.apache.commons.lang3.StringUtils;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Modified;
+import org.osgi.service.component.annotations.Reference;
 
 import com.liferay.asset.kernel.model.AssetEntry;
-import com.liferay.asset.kernel.service.AssetEntryLocalServiceUtil;
+import com.liferay.asset.kernel.service.AssetEntryLocalService;
 import com.liferay.document.library.kernel.model.DLFileEntry;
-import com.liferay.document.library.kernel.service.DLFileEntryLocalServiceUtil;
+import com.liferay.document.library.kernel.service.DLFileEntryLocalService;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.exception.ModelListenerException;
 import com.liferay.portal.kernel.exception.PortalException;
@@ -78,7 +79,7 @@ public class ClassifyingDocumentListener extends BaseModelListener<AssetEntry> {
 
 		// Get the uploaded file entry
 		DLFileEntry fileEntry =
-			DLFileEntryLocalServiceUtil.fetchDLFileEntry(model.getClassPK());
+			getDLFileEntryLocalService().fetchDLFileEntry(model.getClassPK());
 
 		// Check if we were able to get the file
 		if (fileEntry == null) {
@@ -124,7 +125,7 @@ public class ClassifyingDocumentListener extends BaseModelListener<AssetEntry> {
 
 		// Apply received predictions as tags for this image
 		try {
-			AssetEntryLocalServiceUtil.updateEntry(
+			getAssetEntryLocalService().updateEntry(
 				model.getUserId(), model.getGroupId(),
 				DLFileEntry.class.getName(), model.getClassPK(),
 				model.getCategoryIds(),
@@ -164,6 +165,13 @@ public class ClassifyingDocumentListener extends BaseModelListener<AssetEntry> {
 			return null;
 		}
 		
+		// Log the threshold in use
+		if (_log.isDebugEnabled()) {
+			_log.debug(
+				"Predictions will be filtered with the following threshold: " +
+					_configuration.classificationThreshold());
+		}
+
 		for (ClarifaiOutput<Concept> result : clarifaiResults) {
 			for (Concept data : result.data()) {
 				if (_log.isDebugEnabled()) {
@@ -179,6 +187,34 @@ public class ClassifyingDocumentListener extends BaseModelListener<AssetEntry> {
 		}
 		return tagList.toArray(new String[0]);
 	}
+
+	public DLFileEntryLocalService getDLFileEntryLocalService() {
+
+		return _dlFileEntryLocalService;
+	}
+
+	@Reference
+	public void setDLFileEntryLocalService(
+		DLFileEntryLocalService dlFileEntryLocalService) {
+
+		this._dlFileEntryLocalService = dlFileEntryLocalService;
+	}
+
+	public AssetEntryLocalService getAssetEntryLocalService() {
+
+		return _assetEntryLocalService;
+	}
+
+	@Reference
+	public void setAssetEntryLocalService(
+		AssetEntryLocalService assetEntryLocalService) {
+
+		this._assetEntryLocalService = assetEntryLocalService;
+	}
+
+	// Liferay services to be injected through DS
+	private DLFileEntryLocalService _dlFileEntryLocalService;
+	private AssetEntryLocalService _assetEntryLocalService;
 
 	// Configuration Admin object, to get this module configurations
 	private volatile ClassifyingDocumentConfiguration _configuration;
